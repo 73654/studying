4becd246897b206c4b2da5493cabf2a29ff06218
15555573654 <3423567@qq.com>
2023-03-20 17:43:15 +0800

    更新

---
+++ project/grabbag.py
@@ -0,0 +1,53 @@
+import random
+
+
+class Person:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def show(self):
+        print(f"我是{self.name},现在有{self.balance}块钱")
+
+
+class Manager(Person):
+    """群主类"""
+    red_pockets = []
+
+    def sendbag(self, money, num):
+        if money > self.balance:
+            print("我借贷给你们发")
+        self.balance = self.balance - money
+        for i in range(num):
+            avg = money // num
+            mod = money % num
+            for x in range(num):
+                self.red_pockets.append(avg)
+            self.red_pockets[-1] += mod
+            # self.red_pockets.append(money)
+
+class Member(Person):
+    """成员类"""
+
+    def grabbag(self, red_list):
+        if len(red_list) == 0:
+            print("手慢了,红包抢完了")
+            return
+        self.balance += red_list.pop()
+        print(f"{self.name}抢了一个红包!")
+
+
+if __name__ == '__main__':
+    manager = Manager("郗辰政", 1000)
+    manager.show()
+    manager.sendbag(1001,2)
+    manager.show()
+    zelin = Member(name="王泽林", balance=100)
+    shijie = Member(name="耿世杰", balance=100)
+    zelin.show()
+    shijie.show()
+    zelin.grabbag(manager.red_pockets)
+    shijie.grabbag(manager.red_pockets)
+    zelin.show()
+    shijie.show()
+
---
+++ web/testdemo.py
@@ -0,0 +1,20 @@
+# # 导入selenium 包
+# from selenium import webdriver
+# # 创建一个 Chromdriver 的实例。Chrome()会从环境变量中寻找浏览器驱动
+# driver = webdriver.Chrome() # 打开网址
+# driver.get("https://www.baidu.com/")
+# # 关闭driver
+# driver.quit()
+
+# 导入selenium 包
+# from selenium import webdriver
+# 
+# # 创建一个 Geckodriver 的实例。Firefox()会从环境变量中寻找浏览器驱动
+# driver = webdriver.Firefox()
+# # 打开网址
+# driver.get("https://www.baidu.com/")
+# # 关闭driver
+# driver.quit()
+
+s = [1,2,3,4,5,6]
+print(s[1:4:2])
